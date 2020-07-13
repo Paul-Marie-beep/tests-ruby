149ed184844a691534618ba8bf7a9df7bdce2107
Paul-Marie-beep <67626695+Paul-Marie-beep@users.noreply.github.com>
2020-07-13 23:46:26 +0200

    Add files via upload

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -4,9 +4,12 @@ def translate_word(word)
   if  word[0] == "a" || word[0] == "o" || word[0] == "u" || word[0] == "e" # première lettre est une voyelle
     return word+"ay"
   else # La première lettre est une consonne
-    if word[1] == "a" || word[1] == "o" || word[1] == "u" || word[1] == "e" #la première lettre 
+    if word[1] == "a" || word[1] == "o" || word[1] == "u" || word[1] == "e" || word[1] == "i" #la première lettre 
       #est une consonne et pas la deuxième donc ça ne commence que par une seule consonne
-        return word.delete(word[0]) + word[0] + "ay"
+         
+        m = word.delete(word[0])
+      
+        return  m + word[0] + "ay"
 
     else #la première lettre est une consonne et la deuxième aussi => Le mot commence par deux consonnes
       if word[2] == "a" || word[2] == "o" || word[2] == "u" || word[2] == "e"  #Le mot ne commence que par deux consonnes
@@ -25,10 +28,20 @@ end
 
 
 def translate(string)
-  taille = string.size
-  if taille > 1
+  taille = string.split
+  if taille.size < 2
+    trad = translate_word(string)
+    return trad
   else 
-    
+    tableau_trad =[]
+    tableau = string.split
+     tableau.each do |mot|
+      tableau_trad << translate_word(mot)
+    end
+    trad = tableau_trad.join(' ') 
+    return trad
+  end
+end
 
 
 
